a8319c4a7f95dd3b49321ba7388c9e4fbd631494
Azad Ayhan <azadayhan50@gmail.com>
2024-02-14 15:37:47 +0300

    create sign up page ui

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import { Button, Label, TextInput } from "flowbite-react";
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
-  )
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        {/* left side */}
+        <div className="flex-1">
+          <Link to="/" className="font-bold dark:text-white text-4xl">
+            <span className="px-1 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+              Wien
+            </span>
+            Blog
+          </Link>
+          <p className="text-sm mt-5">
+            Möchten Sie sich dieser Reise anschließen? Folgen Sie unseren
+            Social-Media-Konten, um über neue Inhalte informiert zu werden,
+            öffnen Sie Benachrichtigungen und interagieren Sie mit uns, indem
+            Sie Ihre Beiträge teilen. Wie wäre es, Teil der Wien Blog Familie zu
+            werden und gemeinsam die faszinierende Welt von Wien zu entdecken?
+          </p>
+        </div>
+        {/* right side */}
+        <div className="flex-1">
+          <form className="flex flex-col gap-4">
+            <div>
+              <Label value="Your username" />
+              <TextInput type="text" placeholder="Username" id="username" />
+            </div>
+            <div>
+              <Label value="Your email" />
+              <TextInput type="text" placeholder="name@company.com" id="email" />
+            </div>
+            <div>
+              <Label value="Your password" />
+              <TextInput type="text" placeholder="Password" id="password" />
+            </div>
+            <Button gradientDuoTone="purpleToPink" type="submit">Sign Up</Button>
+          </form>
+          <div className="flex gap-2 mt-5 text-sm">
+            <span>Have an account?</span>
+            <Link to="/sign-in" className="text-blue-500">Sign In</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
